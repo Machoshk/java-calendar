dadecac0a328174873eab76c140790113ffd20e1
kshk9 <kshk9@192.168.2.103>
2021-08-23 18:03:49 +0900

    Add repeat function

--- src/sang/calendar/Calendar.java
+++ src/sang/calendar/Calendar.java
@@ -3,43 +3,41 @@ package sang.calendar;
 import java.util.Scanner;
 
 public class Calendar {
-	
-	private final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	public int maxDaysOFMonth(int month) {
+
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public int getMaxDaysOfMonth(int month) {
+
 		return MAX_DAYS[month - 1];
 	}
-	
-	public static void main(String[] args) {
-//		System.out.println("일  월  화  수  목 금 토");
-//		System.out.println("--------------------");
-//		System.out.println(" 1  2  3  4  5  6  7");
-//		System.out.println(" 8  9 10 11 12 13 14");
-//		System.out.println("15 16 17 18 19 20 21");
-//		System.out.println("22 23 24 25 26 27 28");
 
-		
-		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램 
-		// 달을 입력하세요.
-		// 3 (엔터)
-		// 3월은 31일까지 있습니다.
-		String PROMPT ="cal>";
+	public void printSampleCalendar() {
+		System.out.println("일  월  화  수  목 금  토");
+		System.out.println("--------------------");
+		System.out.println(" 1  2  3  4  5  6  7");
+		System.out.println(" 8  9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
+	}
+
+	public static void main(String[] args) {
+
 		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
+
 		System.out.println("반복횟수를 입력하세요.");
-		
-		int month = 1;
-		
-		while (month != -1) {
+		int repeat = scanner.nextInt();
+		for (int i = 0; i < repeat; i++) {
 			System.out.println("달을 입력하세요");
-			System.out.print(PROMPT);
-			month = scanner.nextInt();
-			int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-			System.out.printf("%d월은 %d일까지 있습니다.", month, maxDays[month-1]);
+			int month = scanner.nextInt();
+			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
 		}
 		
 		System.out.println("The End");
+
+		// cal.printSampleCalendar();
+
 		scanner.close();
-		
-	
-		
+
 	}
 }
